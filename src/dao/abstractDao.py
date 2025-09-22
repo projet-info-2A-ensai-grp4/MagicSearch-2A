ee35608308452673be185729ec33786f8e6d77ed
OzeelCharles <charles.ozeel@eleve.ensai.fr>
2025-09-22 20:30:39 +0000

    add abstract class and daughter class for dao layer

---
+++ src/dao/abstractDao.py
@@ -0,0 +1,44 @@
+from abc import ABC, abstractmethod
+import psycopg2
+
+
+class abstractDao(ABC):
+    def __init__(self):
+        self.conn_params = {
+            "host": "postgresql-885217.user-victorjean",
+            "port": 5432,
+            "database": "defaultdb",
+            "user": "user-victorjean",
+            "password": "pr9yh1516s57jjnmw7ll",
+        }
+        self.conn = None
+        self.cursor = None
+
+    def __enter__(self):
+        """Enable context manager support."""
+        self.conn = psycopg2.connect(**self.conn_params)
+        self.cursor = self.conn.cursor()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """Ensure resources are closed."""
+        if self.cursor:
+            self.cursor.close()
+        if self.conn:
+            self.conn.close()
+
+    @abstractmethod
+    def create(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def get_by_id(self, id):
+        pass
+
+    @abstractmethod
+    def update(self, id, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def delete(self, id):
+        pass
---
+++ src/dao/userDao.py
@@ -0,0 +1,7 @@
+import psycopg2
+import warnings
+from psycopg2 import sql
+from abstractDao import abstractDao
+
+class userDao(abstractDao):
+    pass

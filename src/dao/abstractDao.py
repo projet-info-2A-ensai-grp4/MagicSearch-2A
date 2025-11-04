4492e7be9f140df1353688bd3850b0457a311229
OzeelCharles <charles.ozeel@eleve.ensai.fr>
2025-11-04 09:15:27 +0000

    patching abstractDao (connexion function)

--- src/dao/abstractDao.py
+++ src/dao/abstractDao.py
@@ -1,33 +1,25 @@
 from abc import ABC, abstractmethod
-import psycopg2
+from dbconnection import dbConnection
 
 
 class AbstractDao(ABC):
     def __init__(self):
-        self.conn_params = {
-            # not a problem bc the postgres is only availabe through local
-            # network
-            "host": "postgresql-885217.user-victorjean",
-            "port": 5432,
-            "database": "defaultdb",
-            "user": "user-victorjean",
-            "password": "pr9yh1516s57jjnmw7ll",
-        }
         self.conn = None
         self.cursor = None
 
     def __enter__(self):
-        """Enable context manager support."""
-        self.conn = psycopg2.connect(**self.conn_params)
+        """Utilisation du context manager de dbConnection"""
+        self.db = dbConnection()
+        self.conn = self.db.__enter__()  # ouvre la connexion via dbConnection
         self.cursor = self.conn.cursor()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        """Ensure resources are closed."""
+        """Ferme proprement le curseur et la connexion"""
         if self.cursor:
             self.cursor.close()
-        if self.conn:
-            self.conn.close()
+        # on délègue la fermeture à dbConnection
+        self.db.__exit__(exc_type, exc_val, exc_tb)
 
     @abstractmethod
     def exist(self, id):
